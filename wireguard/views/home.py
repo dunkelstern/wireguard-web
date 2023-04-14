1130e67ca72b5e9b49afd48de5161933f8ee5278
Johannes Schriewer <hallo@dunkelstern.de>
2023-04-15 00:39:32 +0200

    Fix some UI stuff and make QR-Code not take up whole screen

--- wireguard/views/home.py
+++ wireguard/views/home.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from wireguard.models import WireguardServerSelfRegistration
+from wireguard.models import WireguardClient, WireguardServer, WireguardServerSelfRegistration
 
 
 class HomeView(TemplateView):
@@ -13,3 +13,11 @@ class HomeView(TemplateView):
             domains.add(reg.email_domain)
         context["self_registrations"] = list(domains)
         return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.user.is_authenticated:
+            _, domain = request.user.email.split("@")
+            context["servers"] = WireguardServer.objects.filter(self_registrations__email_domain=domain)
+            context["clients"] = WireguardClient.objects.filter(owner=request.user)
+        return self.render_to_response(context)
