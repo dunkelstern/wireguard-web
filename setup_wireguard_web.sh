8f5a5af0f6281dec7b8b8e472ff92599c3f1d67c
Johannes Schriewer <j.schriewer@anfe.ma>
2023-04-14 15:59:05 +0200

    More templating in setup script
    Fixes #3

--- setup_wireguard_web.sh
+++ setup_wireguard_web.sh
@@ -10,12 +10,23 @@ if [ $(id -u) -gt 0 ] ; then
 fi
 
 mkdir -p /etc/wireguard-web
+
+# some path templating
 path=$(pwd)
+dnsmasq=${which dnsmasq}
+wg=${which wg}
+wgquick=${which wg-quick}
 
 for file in wireguard-web-config.path wireguard-web-config.service wireguard-web-dnsmasq@.service wireguard-web-wg-quick@.service ; do
     tempfile=$(mktemp /tmp/service.XXXXXXXX)
-    sed -e "s@{path}@${path}@" -e "s@{config}@${WIREGUARD_STAGING_CONFIG_DIRECTORY}@" systemd/$file >$tempfile
-    install -o root -g root -m 0640 $tempfile /etc/systemd/system/$file
+    sed \
+        -e "s@{path}@${path}@" \
+        -e "s@{dnsmasq}@${dnsmasq}@" \
+        -e "s@{wg}@${wg}@" \
+        -e "s@{wgquick}@${wgquick}@" \
+        -e "s@{config}@${WIREGUARD_STAGING_CONFIG_DIRECTORY}@" \
+        systemd/$file >$tempfile
+    install -o root -g root -m 0644 $tempfile /etc/systemd/system/$file
     rm $tempfile
 done
 
